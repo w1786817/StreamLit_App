97efff9e403f51f9fc4ff1a62bf10d5e18fefbf0
w1786817 <132316221+w1786817@users.noreply.github.com>
2024-09-01 16:17:35 +0100

    Update app.py

--- app.py
+++ app.py
@@ -72,12 +72,18 @@ if uploaded_file is not None:
         st.write("")
         st.header("Geospatial Analysis with Map Markers")
 
+        #Init map
+        m = folium.Map()
+
         # Extract latitude and longitude
         final_df['latitude'] = final_df['geo.coordinates'].apply(lambda x: x[0])
         final_df['longitude'] = final_df['geo.coordinates'].apply(lambda x: x[1])
 
-        # Create a base map centered around the mean latitude and longitude
-        m = folium.Map(location=[final_df['latitude'].mean(), final_df['longitude'].mean()], zoom_start=10)
+        min_lat, max_lat = final_df['latitude'].min(), final_df['latitude'].max()
+        min_lon, max_lon = final_df['longitude'].min(), final_df['longitude'].max()
+
+        # Adjust the map to fit all coordinates using the fit_bounds method
+        m.fit_bounds([[min_lat, min_lon], [max_lat, max_lon]])
 
         # Add a marker for each location in the DataFrame
         for idx, row in final_df.iterrows():
